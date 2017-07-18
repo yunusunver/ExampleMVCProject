447cdc217e04deef5959830a15edcd561dfa583d
yunusunver <yunus.unver1995@gmail.com>
2017-07-18 11:58:59 +0300

    First Commit

---
+++ ExampleMVCProject/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ExampleMVCProject.Models;
+namespace ExampleMVCProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+       
+        public JsonResult RGBColor()
+        {
+            RGB color=new RGB();
+            return Json(color);
+        }
+	}
+}
---
+++ ExampleMVCProject/Models/RGB.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExampleMVCProject.Models
+{
+    public class RGB
+    {
+        public string Red = "#FF0000";
+        public string Green = "#00FF00";
+        public string Blue = "#0000FF";
+    }
+}
